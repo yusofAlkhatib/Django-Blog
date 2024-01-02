b17281ba81d3adcbe90dcdeb6fc75c9a2536979d
yusofalkhatib <yusof94@hotmail.com>
2024-01-02 18:53:24 +0100

    add summernote editor

--- posts/admin.py
+++ posts/admin.py
@@ -1,15 +1,14 @@
 
 from django.contrib import admin
-
-
-
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ['id','name','creatrd_at']
     list_filter = ['creatrd_at']
     search_fields = ['name','content']
+    summernote_fields = '__all__'
 
 admin.site.register(Post,PostAdmin)
 
